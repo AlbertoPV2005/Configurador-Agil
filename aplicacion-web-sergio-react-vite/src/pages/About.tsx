ae6c6812156692beb2a1b8bf77054073596ff6d7
SergioInetum <sergio222amador@gmail.com>
2025-10-17 10:59:27 +0200

    [FRONTEND] <17/10/25> MainBase - Se crea el proyecto base con VIte, Axios y FluentUI y TS para poder empezar a trabajar
    [FRONTEND] <17/10/25> MainBase - Se crea el proyecto base con VIte, Axios y FluentUI y TS para poder empezar a trabajar

---
+++ aplicacion-web-sergio-react-vite/src/pages/About.tsx
@@ -0,0 +1,13 @@
+// src/pages/About.tsx
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>Acerca de nosotros</h1>
+      <p>Esta es una página de ejemplo.</p>
+    </div>
+  );
+};
+
+export default About;
