96c2a5afecbb264562e417a671ea4e68fefa8566
MarksitoR <mark.machado.b@gmail.com>
2025-10-22 09:54:08 +0200

    [FrontEnd] <22/10/25> Redirección home

--- aplicacion-web-sergio-react-vite/src/App.jsx
+++ aplicacion-web-sergio-react-vite/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/PaginaVinilos';
 import './styles/App.css';  // Importa estilos CSS
 
 function App() {
